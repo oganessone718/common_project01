69afcc663a1aa3395b93e681b2692b7d896e7955
오가네손 <105881036+oganessone718@users.noreply.github.com>
2024-01-03 18:47:05 +0900

    done

--- app/src/main/java/com/example/common_project01/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/common_project01/ui/home/HomeFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.content.Context
 import android.content.CursorLoader
 import android.icu.util.Calendar
+import java.util.Date
 import android.net.Uri
 import android.os.Build
 import android.provider.DocumentsContract
@@ -328,6 +329,16 @@ class HomeFragment : Fragment() {
             saveBtn.isEnabled = true
         }
     }
+    fun stringToDate(str: String): Date? {
+        val format = SimpleDateFormat("yyyy-MM-dd")
+        return try {
+            format.parse(str)
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
+        }
+    }
+
     // onCreateView: Fragment의 뷰를 생성할 때 호출하는 메서드
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -367,7 +378,7 @@ class HomeFragment : Fragment() {
         }
 
         calendartoday = view.findViewById(R.id.mcalendarView)
-        calendartoday.setSelectedDate(CalendarDay.today())
+        calendartoday.setSelectedDate(stringToDate(selectedDate))
         // 이미지가 있는 날짜에만 EventDecorator 적용
         if (datesWithImage.isNotEmpty()) {
             calendartoday.addDecorator(EventDecorator(datesWithImage))
@@ -386,7 +397,6 @@ class HomeFragment : Fragment() {
             findNavController().navigate(R.id.navigation_home, bundle)
         }
         selectedDate = arguments?.getString("onDate") ?: formatDate(calendar.get(Calendar.YEAR), calendar.get(Calendar.MONTH), calendar.get(Calendar.DAY_OF_MONTH))
-
 
         val format = SimpleDateFormat("yyyy-MM-dd", Locale.getDefault())
         val date = format.parse(selectedDate) // Date 객체
